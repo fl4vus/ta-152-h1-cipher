54d84ddf76251c652cd3c861ec8c6eee53a48b2f
Riyan <88957213+fl4vus@users.noreply.github.com>
2024-05-14 19:38:24 +0530

    Update manp.h

--- manp.h
+++ manp.h
@@ -4,6 +4,16 @@ using namespace std;
 
 void manp()
 {
-	cout << "this is a work in progress, stay tuned :)" << endl;
+	cout << "\nthis is a work in progress, some features aren't fully implemented yet, stay tuned :)" << endl;
+
+	cout << "USAGE: ./t152h1 [PARAM1] [PARAM2] [PARAM3 (Conditional)]\n" << endl;
+
+	cout << "\nValid [PARAM1] Values & Their Purpose: \n" << "1. realtime: encode/decode text after being prompted in the terminal itself. \n" << "2. readfile: encode/decode text sampled from a plain text file." << endl;
+
+	cout << "\nValid [PARAM2] Values & Their Purpose: \n" << "1. encode: encode the sampled input string using the 4-digit numeric key. \n" << "2. decode: decode the sampled input string using the 4-digit numeric key." << endl;
+
+	cout << "\n[PARAM3] is only used when [PARAM1] == readfile. Use [PARAM3] to specify the input file." << endl;
+
+	cout << "\nKEY: The key is a 4-digit numeric PIN which is used to encode/decode. While encoding, it is recommended to not repeat digits, although it has marginal impact in most cases." << endl;
 
 }
