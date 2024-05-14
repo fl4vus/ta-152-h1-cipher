20fef4124bb6860764c86cf1c4d116810319286a
Riyan <88957213+fl4vus@users.noreply.github.com>
2024-05-14 18:56:16 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -32,6 +32,8 @@ int main(int argc, char** argv)
     int index[mainsize];
     int key;
     int keyArr[4];
+    int decKey;
+    int decKeyArr[4] = {0, 0, 0, 0};
 
     int mode;
     int effectivesize;
@@ -40,7 +42,7 @@ int main(int argc, char** argv)
     char sample[SAMPLE_LIMIT];
     char scrambled[SAMPLE_LIMIT];
 
-    if (!(argc == 2))
+    if (!(argc == 3))
     {
         fprintf(stderr, "ERROR: Invalid Arguments!\n");
         manp();
@@ -49,17 +51,24 @@ int main(int argc, char** argv)
 
     else
     {
-        if (strcmp(argv[1], "realtime") == 0)
+        if ((strcmp(argv[1], "realtime") == 0) && (strcmp(argv[2], "encode") == 0))
         {
             mode = 0;
-            cout << "Ta-152-H Encrypter\n" << string(18, '-') << endl << "By RIYAN DAHIYA :]\n" << endl;
-            cout << "Using User Input for Sampling" << endl;
+            cout << "Ta-152-H1 Encrypter\n" << string(19, '-') << endl << "By: RIYAN DAHIYA :]\n" << endl;
+            cout << "Using User Input for Encoding" << endl;
         }
+        else if ((strcmp(argv[1], "realtime") == 0) && (strcmp(argv[2], "decode") == 0))
+        {
+            mode = 1;
+            cout << "Ta-152-H1 Encrypter\n" << string(19, '-') << endl << "By: RIYAN DAHIYA :]\n" << endl;
+            cout << "Using User Input for Decoding" << endl;
+        }
+        
         
         else if (strcmp(argv[1], "readfile") == 0)
         {
-            mode = 1;
-            cout << "Ta-152-H Encrypter\n" << string(18, '-') << endl << "By RIYAN DAHIYA :]\n" << endl;
+            mode = 2;
+            cout << "Ta-152-H1 Encrypter\n" << string(19, '-') << endl << "By: RIYAN DAHIYA :]\n" << endl;
             cout << "Using Text File for Sampling" << endl;
         }
 
@@ -79,10 +88,16 @@ int main(int argc, char** argv)
         break;
 
     case 1:
+        cout << "\nEnter Input String: \n";
+        fgets(sample, SAMPLE_LIMIT, stdin);
+        break;
+
+    case 2:
         manp();
         break;
 
     default:
+        mode = 0;
         cout << "\nOk... Enter Input String: \n";
         fgets(sample, SAMPLE_LIMIT, stdin);
         //cout << "\nINPUT: " << sample << endl;
@@ -118,21 +133,52 @@ int main(int argc, char** argv)
     keyArr[2] = (key%100)/10;
     keyArr[3] = key%10;
 
-    for (int i = 0; i < 4; i++)
+    if (mode == 0)
     {
-        superShuffle(index, keyArr[i], mainsize);
+        for (int i = 0; i < 4; i++)
+        {
+            superShuffle(index, keyArr[i], mainsize);
+        }
+        
+        for (int i = 0; i < effectivesize - 1; i++)
+        {
+            int buf1 = findCharPosition(characters, mainsize, sample[i]);
+            int buf2 = findMapPosition(index, mainsize, buf1);
+            char buf3 = characters[buf2];
+            //cout << i << "\t";
+            scrambled[i] = buf3;
+            //cout << sample[i] << "\t" << scrambled[i] << "\n";
+        }
     }
-    
-    for (int i = 0; i < effectivesize; i++)
+
+    if (mode == 1)
     {
-        int buf1 = findCharPosition(characters, mainsize, sample[i]);
-        int buf2 = findMapPosition(index, mainsize, buf1);
-        char buf3 = characters[buf2];
-        //cout << i << "\t";
-        scrambled[i] = buf3;
-        //cout << sample[i] << "\t" << scrambled[i] << "\n";
+        for (int i = 0; i < 4; i++)
+        {
+            decKeyArr[0] = keyArr[3-i];
+
+            //cout << "\n" << decKeyArr[0] << decKeyArr[1] << decKeyArr[2] << decKeyArr[3] << endl;
+            strcpy(scrambled, sample);
+
+            for (int i = 0; i < 4; i++)
+            {
+                superShuffle(index, decKeyArr[i], mainsize);
+            }
+            
+            for (int i = 0; i < effectivesize - 1; i++)
+            {
+                int buf1 = findCharPosition(characters, mainsize, scrambled[i]);
+                int buf2 = findMapPosition(index, mainsize, buf1);
+                char buf3 = characters[buf2];
+                //cout << i << "\t";
+                scrambled[i] = buf3;
+                //cout << sample[i] << "\t" << scrambled[i] << "\n";
+            }
+
+            //cout << "\nOUTPUT: \n" << scrambled << endl;
+        }
+
     }
-    
     cout << "\nOUTPUT: \n" << scrambled << endl;
 
     return 0;
